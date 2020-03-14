a57c7fb1f7ea0382880325ea7f342bd4d59283de
salmi3d <salmi3d@gmail.com>
2020-03-14 20:17:33 +0200

    first draft

--- server.js
+++ server.js
@@ -1,9 +1,35 @@
 require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
+const articleRouter = require('./routes/articles')
 const app = express()
 
+mongoose.connect('mongodb://localhost/blog', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+app.set('view engine', 'ejs')
+
+app.use('/articles', articleRouter)
+
 app.get('/', (req, res) => {
-  res.send('test')
+  const articles = [
+    {
+      title: 'Test article',
+      createdAt: new Date(),
+      description: 'Test desc'
+    },
+    {
+      title: 'Test article2',
+      createdAt: new Date(),
+      description: 'Test desc2'
+    }
+  ]
+  res.render('layout', {
+    view: 'all_articles',
+    articles
+  })
 })
 
 app.listen(process.env.PORT)
