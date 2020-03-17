b1fddc92d11aab7cc72f2b8659110600592bd49e
salmi3d <salmi3d@gmail.com>
2020-03-17 09:08:45 +0200

    added readme

---
+++ README.md
@@ -0,0 +1,25 @@
+# pure-blog
+
+Simple markdown blog written with Node.js, Express, Bootstrap and MongoDB
+
+## Demo
+
+Demo lives here: [https://pure-blog.salmi3d.now.sh](https://pure-blog.salmi3d.now.sh)
+
+## Setup
+
+```
+yarn install
+```
+
+## Run for development
+
+```
+yarn dev
+```
+
+### Run for production
+
+```
+yarn start
+```
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require("dotenv").config()
+require('dotenv').config()
 const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
@@ -10,7 +10,7 @@ const posts = require('./api/posts.route')
 const app = express()
 
 app.use(cors())
-process.env.NODE_ENV !== "production" && app.use(morgan("dev"))
+process.env.NODE_ENV !== 'production' && app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
@@ -26,6 +26,6 @@ app.set('view engine', 'ejs')
 
 app.use('/posts', posts.router)
 app.use('/', posts.index)
-app.use("*", (req, res) => res.status(404).json({ error: "not found" }))
+app.use('*', (req, res) => res.status(404).json({ error: 'not found' }))
 
 app.listen(process.env.PORT || 3000)
